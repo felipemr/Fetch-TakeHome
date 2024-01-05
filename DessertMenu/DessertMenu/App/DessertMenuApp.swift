825d6e78b016fd3f70f6b68b9d13fcb4d3111683
felipemr <felipemr94@outlook.com>
2024-01-04 23:21:59 -0300

    Project setup

---
+++ DessertMenu/DessertMenu/App/DessertMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertMenuApp.swift
+//  DessertMenu
+//
+//  Created by Felipe Marques on 04/01/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DessertListView()
+        }
+    }
+}
---
+++ DessertMenu/DessertMenu/DessertListView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DessertMenu
+//
+//  Created by Felipe Marques on 04/01/24.
+//
+
+import SwiftUI
+
+struct DessertListView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Dessert")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    DessertListView()
+}
